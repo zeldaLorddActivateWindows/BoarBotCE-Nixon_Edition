94eaaab2d494d8b6fd2e277e5ba286dc382bd11d
WeslayCodes <60010287+WeslayCodes@users.noreply.github.com>
2023-09-16 22:23:49 -0500

    File got left behind!

--- src/main/typescript/bot/data/global/QuestData.ts
+++ src/main/typescript/bot/data/global/QuestData.ts
@@ -9,5 +9,7 @@
 
 export class QuestData {
     questsStartTimestamp = 0;
+
+    // All 7 quests in ID form, with easiest coming first and hardest coming last
     curQuestIDs: [string, string, string, string, string, string, string] = ['', '', '', '', '', '', '']
 }
